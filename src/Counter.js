d0369a905de58632745d8c49864a3625e67480ba
Carlos Gonzalez <caal.0522@gmail.com>
2019-01-21 14:18:10 -0500

    Adds example with setting specific Value.
    - Bumps mini-lake version.

--- src/Counter.js
+++ src/Counter.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 import { connect } from 'mini-lake'
 
-const Counter = ({ counter, addToCounter }) => {
+const Counter = ({ counter, addToCounter, setCounter }) => {
+  const setTo100 = () => {
+    setCounter(100)
+  }
   return (
     <div>
       <p>{counter}</p>
       <button onClick={addToCounter}>Add</button>
+      <button onClick={setTo100}>Set to 100</button>
     </div>
   )
 }
--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 export const initialState = { counter: 0 }
 
 export const actions = {
-  addToCounter: state => ({ counter: state.counter + 1 })
+  addToCounter: state => ({ counter: state.counter + 1 }),
+  setCounter: (state, count) => {
+    return { ...state, counter: count }
+  }
 }
